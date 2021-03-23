e8070dcbefeea916ac5552129e8f0bbab304fe6e
Krzysztof Funkowski <funkowski.krzysztof@gmail.com>
2021-03-23 15:58:19 +0100

    Added basic api and sender functionalities

---
+++ ProductSender/ProductSender/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http;
+using ProductSender.Models;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace ProductSender
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Product product = new Product();
+            product.nazwa = "Chleb";
+            product.cena = 2.50;
+            await PostProduct(product);
+
+        }
+
+        private static async Task PostProduct(Product product)
+        {
+            string jsonProduct = JsonConvert.SerializeObject(product);
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:5001/");
+            var response = await client.PostAsync("api/product", new StringContent(jsonProduct, Encoding.UTF8, "application/json"));
+            if(response != null)
+            {
+                Console.WriteLine(response.ToString());
+            }
+        }
+    }
+}
